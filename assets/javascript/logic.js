102889212c5178645d2c8e87aabf7e815daf7699
Collin Cavallo <collincavallo@gmail.com>
2017-10-29 13:24:47 -0500

    finished logic, possible style updates to come

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,3 +1,4 @@
+//firebase config file
 var config = {
     apiKey: "AIzaSyDc1mlbavR2h_d2FQfWNEfumsAhwlfkEUE",
     authDomain: "train-time-eb94e.firebaseapp.com",
@@ -6,13 +7,14 @@ var config = {
     storageBucket: "train-time-eb94e.appspot.com",
     messagingSenderId: "236590173785"
 };
+//initialize firebasae
 firebase.initializeApp(config);
 
-
+//creating variables for firebase database and user inputs on form
 var database = firebase.database();
 var trainTime =""
 var frequency = ""
-var timeFormat = 
+//on click for submit button to pull all of the input values of the form and store them into indiviual variables
 $("#submit-button").on("click", function (e) {
     e.preventDefault();
     var trainName = $("#trainName").val().trim();
@@ -20,6 +22,7 @@ $("#submit-button").on("click", function (e) {
     trainTime = $("#trainTime").val().trim();
     frequency = $("#frequency").val().trim();
 
+    //creating a new child of the freshly made variables of the input values into my firebase databasae everytime submit button is pushed
     database.ref().push({
         trainName: trainName,
         destination: destination,
@@ -28,19 +31,27 @@ $("#submit-button").on("click", function (e) {
     });
 
 });
-    
+    //listener for when a new child is created.
 database.ref().on("child_added", function(data){
-    var freq = data.val().frequency;
+//creating variables for the value of the most recent child added for frequency and the first train time
+    var trainFreq = data.val().frequency;
+    var Tfirst = data.val().trainTime
+
+    // edit* simplified all of the math for the time left(shout out to my mom for the help in the math here)
+    var timeConverted = moment(Tfirst, "HH:mm");
+    var diff = moment().diff(moment(Tfirst), "minutes");
+    var timeremain = diff % trainFreq;
+    var timeLeft = trainFreq - timeremain;
+    var trainNext = moment().add(timeLeft, "minutes");
+    var tNext = moment(trainNext).format("HH:mm");
     
-   
+   // creating a new row for everytime a child is added and writing the values needed into the dom on the table.
     var createRow = $("<tr>")
 
     createRow.html(
         "<td>" + data.val().trainName + "</td>" +
         "<td>" + data.val().destination + "</td>" + 
-        "<td>" + data.val().frequency + "</td>" +
-        "<td>" + moment(freq).format("m")) + "</td>" +
-        "<td>" + moment(freq).diff(moment(), "years") + "</td>";
+        "<td>" + data.val().frequency + "</td>" + "<td>" + tNext + "</td>" + "<td>" + timeLeft + "</td>" );
     $("#tableBody").append(createRow)
-    
+
 });
